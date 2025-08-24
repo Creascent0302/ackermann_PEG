44e431150903f81b6386cd1027e915c8de7fed8a
Cao Yueming <118057418+THUGary@users.noreply.github.com>
2025-08-24 13:48:42 +0800

    Add files via upload

---
+++ special_obstacles.py
@@ -0,0 +1,8 @@
+SHAPAN = [(0, 0)] + [(39,0)]+[(0,19)]+[(x, y) for x in range(7, 20) for y in range(2,9)]\
++[(x,y)for x in range (23,30) for y in range (2,9)]\
++[(x,y) for x in range(32,36) for y in range(2,9)]\
++[(x,y) for x in range(23,30) for y in range(12,18)]\
++[(x,y) for x in range(32,36) for y in range(12,18)]\
++[(x,y) for x in range(6,10) for y in range(12,18)]\
++[(x,y) for x in range(16,20) for y in range(12,18)]\
++[(x,y) for x in range(10,16) for y in range(13,17)]
